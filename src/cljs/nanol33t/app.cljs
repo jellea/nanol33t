4112a1940a53dc93ea4dd4864ab81948acf9cf11
Jelle Akkerman <m@jelle.io>
2015-09-26 13:41:23 +0200

    adding initial data structure

--- src/cljs/nanol33t/app.cljs
+++ src/cljs/nanol33t/app.cljs
@@ -1,18 +1,34 @@
 (ns nanol33t.app
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as r]))
 
-(defn some-component []
+(def !global (r/atom {:tempo       120
+                     :instrument-selected :a
+                     :instruments {:a {:selected-pattern 0
+                                       :patterns [{:pattern-length 16
+                                                   :pattern        [64 nil nil 64 64 nil nil 64 64 nil nil 64 64 nil nil 64]}]}
+                                   :c {}
+                                   :b {}}}))
+
+(defn step-view []
+  [:button "bla"])
+
+(defn pattern-view []
+  (let [selected-instrument (get-in @!global [:instruments (:instrument-selected @!global)])
+        selected-pattern    (get-in selected-instrument [:patterns (:selected-pattern selected-instrument)])]
+       (js/console.log selected-pattern)
+       [:div
+        (for [step (:pattern selected-pattern)]
+             [step-view])]))
+
+(defn instrument-selector [instrument-name]
+  [:button (name (key instrument-name))])
+
+(defn root-component []
   [:div
-   [:h3 "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red"]
-    " text."]])
-
-(defn calling-component []
-  [:div "Parent component"
-   [some-component]])
+   [pattern-view]
+   (for [x (:instruments @!global)]
+        [instrument-selector x])])
 
 (defn init []
-  (reagent/render-component [calling-component]
+  (r/render-component [root-component]
                             (.getElementById js/document "container")))
